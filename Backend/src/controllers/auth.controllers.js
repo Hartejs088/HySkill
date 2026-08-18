69b622e1e74449e6c5e5f4f805b5436742a23c5d
Hartej Singh <hartejs088@gmail.com>
2026-08-18 23:21:30 +0530

    Refactor Google Auth to use environment variables

--- Backend/src/controllers/auth.controllers.js
+++ Backend/src/controllers/auth.controllers.js
@@ -1,21 +1,21 @@
-import { generateJWTToken_email, generateJWTToken_username } from "../utils/generateJWTToken.js";
+import {
+  generateJWTToken_email,
+  generateJWTToken_username,
+} from "../utils/generateJWTToken.js";
+
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import { User } from "../models/user.model.js";
 import { UnRegisteredUser } from "../models/unRegisteredUser.model.js";
-import dotenv from "dotenv";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-
-dotenv.config();
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://hyskill-1.onrender.com/auth/google/callback",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
     },
     async (accessToken, refreshToken, profile, done) => {
       done(null, profile);
@@ -28,40 +28,88 @@ export const googleAuthHandler = passport.authenticate("google", {
 });
 
 export const googleAuthCallback = passport.authenticate("google", {
-  failureRedirect: "https://hy-skill.vercel.app/login",
+  failureRedirect: `${process.env.FRONTEND_URL}/login`,
   session: false,
 });
 
 export const handleGoogleLoginCallback = asyncHandler(async (req, res) => {
-  console.log("\n******** Inside handleGoogleLoginCallback function ********");
-  // console.log("User Google Info", req.user);
+  console.log(
+    "\n******** Inside handleGoogleLoginCallback function ********"
+  );
 
-  const existingUser = await User.findOne({ email: req.user._json.email });
+  const email = req.user._json.email;
+
+  const existingUser = await User.findOne({ email });
 
   if (existingUser) {
     const jwtToken = generateJWTToken_username(existingUser);
-    const expiryDate = new Date(Date.now() + 1 * 60 * 60 * 1000);
-    res.cookie("accessToken", jwtToken, { httpOnly: true, expires: expiryDate, secure: true,sameSite: "None"});
-    return res.redirect(`https://hy-skill.vercel.app/discover`);
+
+    const expiryDate = new Date(
+      Date.now() + 1 * 60 * 60 * 1000
+    );
+
+    res.cookie("accessToken", jwtToken, {
+      httpOnly: true,
+      expires: expiryDate,
+      secure: true,
+      sameSite: "None",
+    });
+
+    return res.redirect(
+      `${process.env.FRONTEND_URL}/discover`
+    );
   }
 
-  let unregisteredUser = await UnRegisteredUser.findOne({ email: req.user._json.email });
+  let unregisteredUser = await UnRegisteredUser.findOne({
+    email,
+  });
+
   if (!unregisteredUser) {
     console.log("Creating new Unregistered User");
+
     unregisteredUser = await UnRegisteredUser.create({
       name: req.user._json.name,
       email: req.user._json.email,
       picture: req.user._json.picture,
     });
   }
+
   const jwtToken = generateJWTToken_email(unregisteredUser);
-  const expiryDate = new Date(Date.now() + 0.5 * 60 * 60 * 1000);
-  res.cookie("accessTokenRegistration", jwtToken, { httpOnly: true, expires: expiryDate, secure: true,sameSite: "None" });
-  return res.redirect("https://hy-skill.vercel.app/register");
+
+  const expiryDate = new Date(
+    Date.now() + 0.5 * 60 * 60 * 1000
+  );
+
+  res.cookie("accessTokenRegistration", jwtToken, {
+    httpOnly: true,
+    expires: expiryDate,
+    secure: true,
+    sameSite: "None",
+  });
+
+  return res.redirect(
+    `${process.env.FRONTEND_URL}/register`
+  );
 });
 
 export const handleLogout = (req, res) => {
-  console.log("\n******** Inside handleLogout function ********");
-  res.clearCookie("accessToken");
-  return res.status(200).json(new ApiResponse(200, null, "User logged out successfully"));
+  console.log(
+    "\n******** Inside handleLogout function ********"
+  );
+
+  res.clearCookie("accessToken", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+  });
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        null,
+        "User logged out successfully"
+      )
+    );
 };
